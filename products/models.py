fc1e92cc7b1c7015dc7081f50af3210e57615d79
rahulsah <rahul1910165@akgec.ac.in>
2023-05-07 12:10:54 +0530

    Not using BaseModelConcept

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+# Concept of DONOT REPEAT YOURSELF is not used.
+# uid,created_at,updated_at is being reused many times in different classes.
+# use basemodel to define common class(model)fields.
+
+class Product(models.Model):
+    uid=models.UUIDField(default=uuid.uuid4,primary_key=True,editable=False)
+    product_name=models.CharField(max_length=100)
+    created_at=models.DateField(auto_created=True)
+    updated_at=models.DateField(auto_created=True)
+
+class ProductImages(models.Model):
+    uid=models.UUIDField(default=uuid.uuid4,primary_key=True,editable=False)
+    product_images=models.ImageField(upload_to="products")
+    created_at=models.DateField(auto_created=True)
+    updated_at=models.DateField(auto_created=True)
